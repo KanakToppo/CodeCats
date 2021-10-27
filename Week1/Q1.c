3110758ecd416c3a5ccb9911d5c31801db992f61
KanakToppo <kanakprakashtoppo@gmail.com>
2021-10-27 19:31:10 +0530

    Revert "quick update"
    This reverts commit da0293ad6dbaa08687b11cdfbcdb94544bc10064.

--- Week1/Q1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{ int num;
-printf("Enter a number\n");
-scanf("%d",&num);
-    (num%2==0) ? printf("even"):printf("odd");// using ternary operator
-    return 0;
-}
-
